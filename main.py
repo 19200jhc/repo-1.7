32dcffbedd9b44f89e1406fdba12d60b7aa25cbe
Alexander Scarlett <127165031+19200jhc@users.noreply.github.com>
2023-03-12 23:11:59 +0000

    Forgot to commit until now, but the program is working.

--- main.py
+++ main.py
@@ -6,3 +6,69 @@
   
   TIP: Use assessment guide to help guide you through this Internal
 '''
+import random
+print('---Battleships---')
+while True:
+  pb = 0
+  rb = 0
+  Turn = 1
+  end = 0
+  while end == 0:
+    print('\nTurn',Turn,'\n')
+    while True:
+      p = input('Load(L), Fire(F), or Block(B)? ')
+      if p != 'L' and p != 'F' and p != 'B':
+        print('Invalid')
+      elif p == 'F' and pb < 1:
+        print('You have no bullets!')
+      else:
+        break
+    r = random.randint(1,3)
+    while True:
+      if r == 2 and rb < 1:
+        r = random.randint(1,3)
+      else:
+        break
+    if p == 'L':
+      pb = pb + 1
+      print('You Loaded')
+    if r == 1:
+      rb = rb + 1
+      print('The Enemy Loaded')
+    if p == 'B':
+      print('You Blocked')
+    if r == 3:
+      print('The Enemy Blocked')
+    if p == 'F':
+      print('You Fired')
+      pb = pb - 1
+      if r == 1:
+        end = 1
+    if r == 2:
+      rb = rb - 1
+      print('The Enemy Fired')
+      if p == 'L':
+        end = 2
+      if p == 'F':
+        end = 3
+    if end > 0:
+      break
+    Turn = Turn + 1
+    print('You have',pb,'Bullets')
+    print('The enemy has',rb,'Bullets')
+  if end == 1:
+    print('You Win')
+  if end == 2:
+    print('You Lose')
+  if end == 3:
+    print('Draw')
+  while True:
+    ag = input('Play Again(Y/N)? ')
+    if ag == 'Y' or ag == 'N':
+      break
+    print('Invalid')
+  if ag == 'Y':
+    continue
+  else:
+    break
+print('\nGoodbye')
