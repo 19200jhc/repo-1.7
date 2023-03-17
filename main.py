4ec86681f07cc117c848f2b879c6d5c61603c564
Alexander Scarlett <127165031+19200jhc@users.noreply.github.com>
2023-03-17 00:13:39 +0000

    more colors, also added comments for code readability

--- main.py
+++ main.py
@@ -6,24 +6,33 @@
   
   TIP: Use assessment guide to help guide you through this Internal
 '''
+#library for picking random numbers
 import random
+#ascii art
 f = open('boat.txt', 'r')
 content = f.read()
 print(content)
 f.close()
 print('\n')
+#initializing score and game variables
 ps = 0
 rs = 0
 g = 1
+#main game loop
 while True:
+  #header
   print('------ Battleships ------\n')
   print(' ------- Game',g,'-------')
+  #initializing ammo, turn and end variables
   pb = 0
   rb = 0
   Turn = 1
   end = 0
+  #single turn loop
   while end == 0:
+    #print turn number
     print('\033[0;32m\n       - Turn',Turn,'-\n\033[0;37m')
+    #input action for turn and check if valid
     while True:
       p = input('\033[0;33mLoad(L)\033[0;37m, \033[0;31mFire(F)\033[0;37m, or \033[0;34mBlock(B)\033[0;37m? ')
       if p != 'L' and p != 'F' and p != 'B':
@@ -32,40 +41,46 @@ while True:
         print('\033[1;31m\nYou have no bullets!\n\033[0;37m')
       else:
         break
+    #random number for enemy turn
     r = random.randint(1,3)
+    #check if valid
     while True:
       if r == 2 and rb < 1:
         r = random.randint(1,3)
       else:
         break
     print()
+    #print actions taken on turn
     if p == 'L':
       pb = pb + 1
-      print('You Loaded')
+      print('You \033[0;33mLoaded\033[0;37m')
     if r == 1:
       rb = rb + 1
-      print('The Enemy Loaded')
+      print('The Enemy \033[0;33mLoaded\033[0;37m')
     if p == 'B':
-      print('You Blocked')
+      print('You \033[0;34mBlocked\033[0;37m')
     if r == 3:
-      print('The Enemy Blocked')
+      print('The Enemy \033[0;34mBlocked\033[0;37m')
     if p == 'F':
-      print('You Fired')
+      print('You \033[0;31mFired\033[0;37m')
       pb = pb - 1
       if r == 1:
         end = 1
     if r == 2:
       rb = rb - 1
-      print('The Enemy Fired')
+      print('The Enemy \033[0;31mFired\033[0;37m')
       if p == 'L':
         end = 2
       if p == 'F':
         end = 3
+    #check if game ended
     if end > 0:
       break
+    #new turn, print amount of ammo
     Turn = Turn + 1
-    print('\nYou have',pb,'Bullets')
-    print('The enemy has',rb,'Bullets')
+    print('\nYour \033[0;33mammunition\033[0;37m:\033[0;36m',pb)
+    print('\033[0;37mThe enemy\'s \033[0;33mammunition\033[0;37m:\033[0;36m',rb)
+  #display ending, change score
   if end == 1:
     ps = ps + 1
     print('\033[0;32m\nYou Win\033[0;37m')
@@ -74,16 +89,20 @@ while True:
     rs = rs + 1
   if end == 3:
     print('\033[0;33m\nDraw\033[0;37m')
-  print('\nThe score is:\nYou: ',ps,' Enemy: ',rs,'\n')
+  #print score
+  print('\nThe \033[0;34mscore\033[0;37m is:\nYou: \033[0;32m',ps,'\033[0;37m Enemy: \033[0;31m',rs,'\033[0;37m\n')
+  #input new game, check if valid
   while True:
-    ag = input('Play Again(Y/N)? ')
+    ag = input('Play Again(\033[0;32mY\033[0;37m/\033[0;31mN\033[0;37m)? ')
     if ag == 'Y' or ag == 'N':
       break
-    print('\nInvalid! Type Y or N!\n')
+    print('\n\033[1;31mInvalid! Type Y or N!\033[0;37m\n')
+  #repeat loop if yes, new game
   if ag == 'Y':
     g = g + 1
     print('\n')
     continue
+  #else break loop, say goodbye
   else:
     break
 print('\nGoodbye')
